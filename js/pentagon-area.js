8e92449501035a19654d7e83ad54a15b8bd038e1
Sabbir Ahmmed <engineersabbir.dev@gmail.com>
2023-08-18 01:02:03 +0600

    pentagon area calculation

---
+++ js/pentagon-area.js
@@ -0,0 +1,12 @@
+function pentagonArea() {
+    const perimeter = getInputValue('pentagon-perimeter');
+    const apothem = getInputValue('pentagon-apothem');
+
+    if (isNaN(perimeter) || isNaN(apothem)) {
+        alert('Please provide a valid number');
+        return;
+    }
+
+    const area = 0.5 * perimeter * apothem;
+    setElementText('pentagon-area', area);
+}
